e2b45d21b1073a5f00daf2f8ccda48db9c82dbe7
Jerry Majors Patterson <jem@apple.com>
2021-10-30 21:41:42 +0000

    Code Cleanup, some error handling, just make the seed word the first word of the input_text, added one more text for testing.

--- markov.py
+++ markov.py
@@ -43,67 +43,59 @@ def make_chains(text_string):
 
     chains = {}
 
-    text_string = text_string.replace("\n", "\n ")
     words = text_string.split(" ")
-    if '' in words:
-        words.remove('')
 
     for i in range(len(words)-2):
         key_tuple = (words[i], words[i+1])
         next_word = words[i+2]
         # https://stackoverflow.com/questions/12905999/python-dict-how-to-create-key-or-append-an-element-to-key
-        if key_tuple in chains:
-            existing_list = chains[key_tuple]
-            existing_list.append(next_word)
-            chains[key_tuple] = existing_list  # existing_list is really a new_list.
-        else:
-            chains[key_tuple] = [next_word] 
+        chains.setdefault(key_tuple,[next_word]).append(next_word)
     return chains
 
 
 def make_text(chains, seed_word):
     """Return text from chains."""
 
-
-    #key_words = choice(list(chains.keys()))
+    possible_seed_tuples = []
     for each_key in chains:
         if each_key[0] == seed_word:
-            next_pair = each_key
-    words = [f" {next_pair[0]} {next_pair[1]}"]
+            possible_seed_tuples.append(each_key)
+    try:
+        seed_tuple = choice(possible_seed_tuples)
+    except IndexError:
+        return f"Uh oh; '{seed_word}' is not a good seed_word for this text."
+    words = [f"{seed_tuple[0]} {seed_tuple[1]}"]
 
     while True:
-
-        #a_random_key_from_chains_dict = choice(list(chains.keys()))
         
-        possible_values = chains.get(next_pair)
+        possible_values = chains.get(seed_tuple)
 
         try:
-            next_pair = (next_pair[1], choice(possible_values))
-            words.append(next_pair[1])
+            seed_tuple = (seed_tuple[1], choice(possible_values))
+            words.append(seed_tuple[1])
         except TypeError:
             break
-
-        #print(words)
-
-    #print(a_random_key_from_chains_dict)
 
     return ' '.join(words)
 
 
-input_path = 'gettysburg.txt'
-seed_word = "Four"
+for input_path in ['gettysburg.txt', 'green-eggs.txt', 'the_boy_who_lived.txt']:
 
+    # Open the file and turn it into one long string
+    try:
+        input_text = open_and_read_file(input_path)
+    except FileNotFoundError:
+        print(f"Uh oh; '{input_path}' is not a good input_path.")
+        break
 
-#input_path = 'green-eggs.txt'
-#seed_word = "Would"
+    # Just make the seed word the first word of the text. Makes text natural-sounding.
+    seed_word = input_text.split(" ")[0]
 
-# Open the file and turn it into one long string
-input_text = open_and_read_file(input_path)
+    # Get a Markov chain
+    chains = make_chains(input_text)
 
-# Get a Markov chain
-chains = make_chains(input_text)
+    # Produce random text
+    random_text = make_text(chains, seed_word)
 
-# Produce random text
-random_text = make_text(chains, seed_word)
-
-print(random_text)
+    divider = "-=" * 40
+    print(f"From {input_path}:\n{random_text}\n\n{divider}-\n")
